ba0405e6c7421fe8ca62b0d1bf4539aff4d6cd8f
Geoff Rowell <geoff.rowell@gmail.com>
2012-04-23 10:51:32 -0400

    Got logging configuration and hook configuration working.

--- SvnHook/StartCommit.py
+++ SvnHook/StartCommit.py
@@ -1,12 +1,33 @@
 #!/usr/bin/env python
 
+import argparse
 from SvnHook import *
 
 class StartCommit(SvnHook):
     """Handles start-commit hook calls."""
 
     def __init__(self):
-        pass
+        """Process the input arguments."""
+
+        # Define how to process the command line.
+        cmdline = argparse.ArgumentParser(
+            description='Handle start-commit hook calls.')
+
+        cmdline.add_argument(
+            '--cfgfile', required=True,
+            help='Path name of the hook configuration file')
+
+        cmdline.add_argument(
+            'repospath', help='Path name of the repository root')
+        cmdline.add_argument(
+            'user', help='Name of the Subversion user')
+        cmdline.add_argument(
+            'capabilities', help='List of client capabilities')
+
+        args = cmdline.parse_args()
+        
+        # Perform parent initialization.
+        super(StartCommit, self).__init__(args.cfgfile)
 
     def run(self):
         raise Exception('Found the handler')
--- SvnHook/SvnHook.py
+++ SvnHook/SvnHook.py
@@ -1,23 +1,51 @@
 #!/usr/bin/env python
+"""Defines SvnHook base class."""
+__author__    = 'Geoff Rowell'
+__copyright__ = 'Copyright 2012, Geoff Rowell'
+__license__   = 'Apache'
+__version__   = '3.00'
+__status__    = 'Development'
+
 import logging
 import logging.config
+import os
+import re
+import yaml
 
 from lxml import etree
 
 class SvnHook(object):
-    """Base class for hook-specific handlers."""
+    """Base class for hook-specific handlers.
 
-    logger = logging.getLogger(__name__)
+    Initializes logging. Reads and parses XML configuration.
+    """
 
-    def __init__(self, **args):
+    def __init__(self, cfgfile):
         """Construct a new object of the class."""
-        
-        # Initialize logging.
-        logging.config.fileConfig(args['logcfgfile'])
+
+        if cfgfile==False:
+            raise KeyError('Required argument missing: cfgfile')
+        if os.path.isfile(cfgfile)==False:
+            raise IOError('Configuration file not found: '
+                            + cfgfile)
+
+        # If a co-located logging configuration file exists, use it.
+        # Prefer a "*-log.yml" file over a "*-log.conf" file.
+        logymlfile = re.sub(
+            r'\.[^\.]+$', r'-log.yml', cfgfile)
+        logconffile = re.sub(
+            r'\.[^\.]+$', r'-log.conf', cfgfile)
+
+        if os.path.isfile(logymlfile)==True:
+            logcfg = yaml.load(open(logymlfile).read())
+            logging.config.dictConfig(logcfg)
+        elif os.path.isfile(logconffile)==True:
+            logging.config.fileConfig(logconffile)
+        else:
+            logging.basicConfig()
 
         # Read and parse the hook configuration file.
-        cfg = open(args['hookcfgfile'])
-        self.cfg = etree.parse(cfg)
+        self.cfg = etree.parse(open(cfgfile))
 
     def run(self):
         raise Exception('Handler not implemented')
